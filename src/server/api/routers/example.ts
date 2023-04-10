5ebccfbbdfdd5fbbf5bb233833f707494e7a6b66
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-10 21:06:56 +0200

    fix typescript error

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -16,7 +16,7 @@ export const exampleRouter = createTRPCRouter({
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.video.findMany();
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
