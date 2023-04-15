b1f644ad1eb00a2240afc7809ab3fc6a6a3b7e16
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-15 21:36:20 +0200

    add test to upload new video

--- tests/videos.spec.ts
+++ tests/videos.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+test.describe.configure({ mode: "serial" });
+
 test("should be able to view videos", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page
@@ -13,4 +15,22 @@ test("no videos should exist", async ({ page }) => {
   await expect(page.locator("div.flex-start > div > span")).toContainText(
     "You do not have any recordings."
   );
+});
+
+test("can upload video", async ({ page }) => {
+  await page.goto("http://localhost:3000/videos");
+  await page.getByText("New video").click();
+  await page
+    .getByText("Drop files to Attach, or browse")
+    .setInputFiles("tests/.auth/example_video.webm");
+  await page.getByRole("button", { name: "Upload" }).click();
+  await expect(page).toHaveURL(/http:\/\/localhost:3000\/share\/[A-Za-z0-9]+/, {
+    timeout: 30000,
+  });
+
+  await page.click('[href="/videos"]');
+  await page.getByText("example_video.webm").click();
+  await expect(page).toHaveURL(/http:\/\/localhost:3000\/share\/[A-Za-z0-9]+/, {
+    timeout: 30000,
+  });
 });
