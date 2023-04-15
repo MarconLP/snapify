f10873b8caca46919fa6d4dff2cd6113315b2fa3
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-15 22:08:42 +0200

    change title input field id from email to title

--- src/components/VideoMoreMenu.tsx
+++ src/components/VideoMoreMenu.tsx
@@ -174,7 +174,7 @@ export default function VideoMoreMenu({ video }: Props) {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <div className="sm:col-span-4">
                     <label
-                      htmlFor="email"
+                      htmlFor="title"
                       className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Video title
@@ -183,10 +183,10 @@ export default function VideoMoreMenu({ video }: Props) {
                       <input
                         value={title}
                         onChange={(e) => setTitle(e.currentTarget.value)}
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
+                        id="title"
+                        name="title"
+                        type="title"
+                        autoComplete="title"
                         className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
