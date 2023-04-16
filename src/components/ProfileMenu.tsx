80bdba7b8924bd8433df9bc56c6c527071ff9e79
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-16 13:23:13 +0200

    fix styling error

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -29,7 +29,7 @@ export default function ProfileMenu() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
             <Menu.Item>
               {({ active }) => (
