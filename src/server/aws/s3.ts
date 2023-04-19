3aec440f1e1f216714a40120d8e27dfab1eaa308
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-19 23:50:00 +0200

    rename AWS_ACCESS_KEY to AWS_SECRET_ACCESS_KEY

--- src/server/aws/s3.ts
+++ src/server/aws/s3.ts
@@ -6,6 +6,6 @@ export const s3 = new S3({
   region: env.AWS_REGION,
   credentials: {
     accessKeyId: env.AWS_KEY_ID,
-    secretAccessKey: env.AWS_ACCESS_KEY,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
   },
 });
