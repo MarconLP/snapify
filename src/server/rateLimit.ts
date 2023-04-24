59b2bdd76eca0f00747de972b0703536dacfe9bf
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-24 11:49:12 +0200

    increase rate limit

--- src/server/rateLimit.ts
+++ src/server/rateLimit.ts
@@ -7,5 +7,5 @@ export const rateLimit = new Ratelimit({
     url: env.UPSTASH_REDIS_REST_URL,
     token: env.UPSTASH_REDIS_REST_TOKEN,
   }),
-  limiter: Ratelimit.slidingWindow(2, "10 s"),
+  limiter: Ratelimit.slidingWindow(60, "60 s"),
 });
