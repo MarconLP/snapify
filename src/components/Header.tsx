b17b8804791ec10c3314465b8272c133d5fdf651
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-30 18:19:29 +0200

    fix duplicate login button in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
 
         <Link
           href="/sign-in"
-          className="text-sm font-semibold leading-6 text-gray-900"
+          className="hidden text-sm font-semibold leading-6 text-gray-900 md:block"
         >
           Log in <span aria-hidden="true">&rarr;</span>
         </Link>
