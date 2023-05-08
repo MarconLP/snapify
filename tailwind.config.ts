11634733eab0155e946fd7c5997b2ac39a4eab7a
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-05-08 16:48:40 +0200

    extend animation and keyframes instead of overwriting them

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,19 +3,20 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    animation: {
-      marquee: "marquee 25s linear infinite",
-      marquee2: "marquee2 25s linear infinite",
-    },
-    keyframes: {
-      marquee: {
-        "0%": { transform: "translateX(0%)" },
-        "100%": { transform: "translateX(-100%)" },
+    extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
       },
-      marquee2: {
-        "0%": { transform: "translateX(100%)" },
-        "100%": { transform: "translateX(0%)" },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
       },
     },
   },
