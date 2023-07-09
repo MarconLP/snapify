3f71ce04db38aada7269dcf840bd20a601d8be67
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-07-09 11:13:28 +0200

    add made by marcus notice into the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,14 @@ export default function Footer() {
     <div className="mx-16 flex items-center justify-center">
       <footer className="mb-4 mt-4 flex h-full w-[1048px] flex-col-reverse items-center justify-between text-sm md:flex-row">
         <div className="my-[50px] ml-8 text-sm text-[#666] md:my-0">
-          © 2023 Snapify
+          © 2023 Snapify by{" "}
+          <Link
+            target="_blank"
+            className="underline"
+            href="https://marcushof.vercel.app/"
+          >
+            Marcus Hof
+          </Link>
         </div>
         <div className="mr-8 flex w-full flex-col sm:px-[50px] md:w-auto md:flex-row md:gap-8 md:px-0">
           {[
