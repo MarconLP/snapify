60516f4f7e60852228b95ff1d58bb43334b3b592
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-07-09 10:36:38 +0200

    add no account needed notice to recorder button

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -410,13 +410,13 @@ export default function Recorder({ closeModal, step, setStep }: Props) {
             </button>
             <button
               type="button"
-              className="ml-auto inline-flex items-center rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold leading-6 text-white shadow transition duration-150 ease-in-out hover:bg-indigo-400 disabled:cursor-not-allowed"
+              className="ml-auto inline-flex items-center rounded-md bg-[#dc2625] px-4 py-2 text-sm font-semibold leading-6 text-white shadow transition duration-150 ease-in-out hover:bg-indigo-400 disabled:cursor-not-allowed"
               onClick={() => {
                 posthog?.capture("recorder: closed post-modal");
                 void closeModal();
               }}
             >
-              Close
+              Delete
             </button>
           </div>
         </div>
