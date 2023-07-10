2edbe063e6b6799da8498b7994f97e31e7e325df
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-07-10 18:29:08 +0200

    remove light mode from tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -11,18 +11,18 @@ const Tooltip = ({ title, children }: Props) => {
     <TooltipPrimitive.Provider delayDuration={0} disableHoverableContent>
       <TooltipPrimitive.Root>
         <TooltipPrimitive.Trigger asChild>
-          <div className="group radix-state-delayed-open:bg-gray-50 radix-state-instant-open:bg-gray-50 radix-state-on:bg-gray-50 radix-state-open:bg-gray-50 dark:radix-state-on:bg-gray-900 dark:radix-state-open:bg-gray-900">
+          <div className="group radix-state-delayed-open:bg-gray-50 radix-state-instant-open:bg-gray-50 radix-state-on:bg-gray-900 radix-state-open:bg-gray-900">
             {children}
           </div>
         </TooltipPrimitive.Trigger>
         <TooltipPrimitive.Content
           sideOffset={4}
           className={
-            "radix-side-top:animate-slide-down-fade radix-side-right:animate-slide-left-fade radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade inline-flex items-center rounded-md bg-white px-4 py-2.5 dark:bg-gray-800"
+            "radix-side-top:animate-slide-down-fade radix-side-right:animate-slide-left-fade radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade inline-flex items-center rounded-md bg-gray-800 px-4 py-2.5"
           }
         >
-          <TooltipPrimitive.Arrow className="fill-current text-white dark:text-gray-800" />
-          <span className="block text-xs leading-none text-gray-700 dark:text-gray-100">
+          <TooltipPrimitive.Arrow className="fill-current text-gray-800" />
+          <span className="block text-xs leading-none text-gray-100">
             {title}
           </span>
         </TooltipPrimitive.Content>
